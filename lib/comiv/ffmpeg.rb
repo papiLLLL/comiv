45a1791c8847808f49f07f564375f8be52e3a617
yuitoku <exrecord160@gmail.com>
2019-10-26 15:40:35 +0900

    Move variables.

--- lib/comiv/ffmpeg.rb
+++ lib/comiv/ffmpeg.rb
@@ -1,14 +1,15 @@
+require_relative "config"
+
 module Comiv::FFmpeg
   VCODEC = "libx264"
   ACODEC = "aac"
   BITRATE = "2500k"
   NULL = "/dev/null"
-  COMPRESS_DIR = "compress"
 
   def self.compress_video(video)
     directory = File.dirname(video)
     file = File.basename(video)
     `ffmpeg -i #{video} -c:v #{VCODEC} -an -pass 1 -f mp4 -loglevel error -y #{NULL}`
-    `ffmpeg -i #{video} -c:v #{VCODEC} -c:a #{ACODEC} -pass 2 -loglevel error -b:v #{BITRATE} #{directory}/#{COMPRESS_DIR}/#{file}`
+    `ffmpeg -i #{video} -c:v #{VCODEC} -c:a #{ACODEC} -pass 2 -loglevel error -b:v #{BITRATE} #{directory}/#{Comiv::COMPRESS_DIRECTORY}/#{file}`
   end
 end
--- lib/comiv/tinify.rb
+++ lib/comiv/tinify.rb
@@ -1,14 +1,12 @@
 require "tinify"
+require_relative "config"
 
 module Comiv::Tinify
-  Tinify.key = nil
-  COMPRESS_DIRECTORY = "compress"
-
   def self.compress_image(image, key)
     Tinify.key = key
     directory = File.dirname(image)
     file = File.basename(image)
     source = Tinify.from_file("#{image}")
-    source.to_file("#{directory}/#{COMPRESS_DIRECTORY}/#{file}")
+    source.to_file("#{directory}/#{Comiv::COMPRESS_DIRECTORY}/#{file}")
   end
 end
